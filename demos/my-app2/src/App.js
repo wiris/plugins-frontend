a6fee683134778c5ae8d51ecc6c202c4cbe1e323
carla-at-wiris <carla@wiris.com>
2024-10-25 10:41:09 +0200

    test: test

---
+++ demos/my-app2/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Ckeditor from "./Ckeditor";
+
+function App() {
+  return (
+    <div className="App">
+      ckeditor
+      <Ckeditor />
+    </div>
+  );
+}
+
+export default App;
